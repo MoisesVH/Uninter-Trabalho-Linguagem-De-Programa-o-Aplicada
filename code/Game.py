0fc7ae66b221d04d0e1db9ac7998645e8ffa5e2d
Moises Viana Honorato <moisesbr2005@gmail.com>
2026-06-06 13:07:14 -0300

    Definindo o Menu Background e audio definido.

--- code/Game.py
+++ code/Game.py
@@ -2,24 +2,21 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WINDOW_WIDTH, WINDOW_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
 
     def run(self, ):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-            # Checando todos os eventos
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # fechando a janela
-            #         print('Fim do programa')
-            #         quit()  # encerrando o pygame
+
 
--- code/Menu.py
+++ code/Menu.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./assets/backgounds/backgound_menu.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer.music.load('./assets/menu.wav')
+        pygame.mixer.music.play(-1)
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            pygame.display.flip()
+
+            # Checando todos os eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # fechando a janela
+                    print('Fim do programa')
+                    quit()  # encerrando o pygame
